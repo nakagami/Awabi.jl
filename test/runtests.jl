6e8ee1ce02ebd912079543fd2550012a73434d74
Hajime Nakagami <nakagami@gmail.com>
2021-01-01 11:07:54 +0900

    add get_dic_path()

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using Test, Awabi
 
 @testset "mecabrc" begin
     @test Awabi.find_mecabrc() == "/etc/mecabrc"
-    @test Awabi.get_mecabrc_map()["dicdir"] == "/var/lib/mecab/dic/debian"
+    mecabrc_map = Awabi.get_mecabrc_map()
+    @test mecabrc_map["dicdir"] == "/var/lib/mecab/dic/debian"
+    @test Awabi.get_dic_path(mecabrc_map, "sys.dic") == "/var/lib/mecab/dic/debian/sys.dic"
 end
 
