13b7e4c98d61a07fcf341ae4762cba9b7a30483c
Hajime Nakagami <nakagami@gmail.com>
2022-11-18 13:08:40 +0900

    fix test script

--- .github/scripts/mk_test_user_dic.sh
+++ .github/scripts/mk_test_user_dic.sh
@@ -7,5 +7,5 @@ DICDIR=/var/lib/mecab/dic/debian
 DIC_SRC=/usr/share/mecab/dic/ipadic
 CMD_IDX="/usr/lib/mecab/mecab-dict-index"
 
-echo -e "ユーザー辞書,,,6058,名詞,一般,*,*,*,*,ユーザー辞書,ユーザージショ,ユーザージショ" > $USERDIC_SRC
+echo "ユーザー辞書,,,6058,名詞,一般,*,*,*,*,ユーザー辞書,ユーザージショ,ユーザージショ" > $USERDIC_SRC
 $CMD_IDX -d $DIC_SRC -u $USERDIC -f utf-8 -t utf-8 $USERDIC_SRC
